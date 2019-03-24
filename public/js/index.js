504fac0d6c324b84e00357932cb491ea1b1dc104
DangenMaster <dangenmaster02@gmail.com>
2019-03-24 07:48:28 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 const socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+  // Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -17,6 +32,7 @@ socket.on('new message', function (message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('new location message', function (message) {
@@ -28,6 +44,7 @@ socket.on('new location message', function (message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
